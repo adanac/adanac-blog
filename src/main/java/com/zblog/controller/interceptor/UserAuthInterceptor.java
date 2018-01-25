231b09b7161fe0479914d26f3e5bd01d677b2b10
adanac <adanac@sina.com>
2018-01-26 01:36:08 +0800

    merge some files

--- src/main/java/com/zblog/controller/interceptor/UserAuthInterceptor.java
+++ src/main/java/com/zblog/controller/interceptor/UserAuthInterceptor.java
@@ -1,41 +1,40 @@
 package com.zblog.controller.interceptor;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.zblog.dmo.User;
+import com.zblog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-import com.zblog.dmo.User;
-import com.zblog.service.UserService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 用户权限拦截
- * @author adanac
  *
+ * @author adanac
  */
 public class UserAuthInterceptor extends HandlerInterceptorAdapter {
 
-	//用户Service
-	@Autowired
-	protected UserService userService;
-	
-	@Override
-    public boolean preHandle(HttpServletRequest  request,HttpServletResponse  response,
-            Object handler)
-    		throws Exception{
-        
-		User user = new User();
-		if (request.getSession().getAttribute("UserID") != null) {			
-			user.setId(Integer.parseInt(request.getSession().getAttribute("UserID").toString()));
-			User temp = userService.getUser(user);
-			if (temp != null) {
-				return true;
-			}
-		}
-		       
+    //用户Service
+    @Autowired
+    protected UserService userService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+                             Object handler)
+            throws Exception {
+
+        User user = new User();
+        if (request.getSession().getAttribute("UserID") != null) {
+            user.setId(Integer.parseInt(request.getSession().getAttribute("UserID").toString()));
+            User temp = userService.getUser(user);
+            if (temp != null) {
+                return true;
+            }
+        }
+
         response.setCharacterEncoding("utf-8");
-        response.sendRedirect(request.getContextPath()+"/user/init-login.htm");
+        response.sendRedirect(request.getContextPath() + "/user/init-login.htm");
         return false;
     }
 
